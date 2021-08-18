5fc423516af8ec3dfe1eb951d5295cf04d2a74f4
jihye-94 <sn022317@gmail.com>
2021-08-19 03:45:36 +0900

    Test JH

--- test/src/Main.java
+++ test/src/Main.java
@@ -5,7 +5,8 @@ public class Main {
     public static void main(String[] args) {
         Psj.printPsj();
         Sjihye.SjihyePrint();
-        Hsj.HsjPrint();
+        Sjihye.SjihyePrint();
+	Hsj.HsjPrint();
 
         System.out.println("yesol");
 
